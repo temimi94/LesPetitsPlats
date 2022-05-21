ec445618b88150e624885b220b0b60e2b35e0e8e
Rogoya Hamdaoui <hamdaouirogoya1994@outlook.fr>
2022-05-21 11:36:11 +0200

    premier push

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,33 +1,44 @@
-/**
- * Filtrer la requête et afficher la correspondance de la carte des recettes avec la requête
- * @param {Array | Object} recipes
- * @param {HTMLElement} searchBar
- */
 const filteredRecipes = (recipes, searchBar) => {
 	searchBar.addEventListener("keyup", (e) => {
 		if (e.target.value.length >= 3) {
+			const results = [];
 			recipesSection.innerHTML = "";
 			const query = e.target.value.toLowerCase();
-			const results = recipes.filter((recipe) => {
-				return (
-					recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query) || recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(query))
-				);
-			});
-			createRecipesCard(results);
+			for (let i = 0; i < recipes.length; i++) {
+				const { name, ingredients, description } = recipes[i];
+				const includesInName = name.toLowerCase().includes(query);
+				const includesInDescription = description.toLowerCase().includes(query);
+				let includesInIngredients = false;
+				for (let y = 0; y < ingredients.length; y++) {
+					if (ingredients[y].ingredient.toLowerCase().includes(query)) {
+						includesInIngredients = true;
+					}
+				}
+				if (includesInName || includesInDescription || includesInIngredients) {
+					results.push(recipes[i]);
+				}
+			}
+
+			if (results.length) {
+				recipesSection.innerHTML = "";
+				createRecipesCard(results);
+			}
+
 			if (!results.length) {
+				recipesSection.innerHTML = "";
 				recipesSection.append(
 					createDom(
 						"div",
 						`Aucune recette ne correspond à votre critère… vous pouvez
-     chercher « tarte aux pommes », « poisson », etc.`,
+						chercher « tarte aux pommes », « poisson », etc.`,
 						{ class: "no__results" }
 					)
 				);
 			}
-		} else if (e.target.value.length <= 3) {
+		}
+		if (e.target.value.length <= 3) {
 			recipesSection.innerHTML = "";
 			createRecipesCard(recipes);
 		}
 	});
 };
-
